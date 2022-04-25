18b010a7aad2a14e91d1ce6ad839c7243567279a
Djrubaraj-Roy <dhrubarajroy1233@gmail.com>
2022-04-25 13:51:02 +0530

    Started learning java

---
+++ first.java
@@ -0,0 +1,12 @@
+class first {
+    int a = 10; //instance variable
+    static int b = 11;  // this is static variable  we can access without creating any object.
+    public static void main(String args[]){
+        System.out.println("How to access instance variable:");
+        // System.out.println(a); error coz if access this a you need to creat a object 
+        first access = new first();
+        System.out.println(access.a);
+        System.out.println(b); //this is static variable  we can access without creating any object.
+    }
+    
+}
